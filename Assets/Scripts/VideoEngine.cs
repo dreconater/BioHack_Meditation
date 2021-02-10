22fb6cd18bf3ed1257df7a87ad28a441ef9d9d6e
Ashot Danielyan <ashot111997@gmail.com>
2021-02-10 15:43:04 +0400

    builded steam version

--- Assets/Scripts/VideoEngine.cs
+++ Assets/Scripts/VideoEngine.cs
@@ -135,7 +135,7 @@ public class VideoEngine : MonoBehaviour
 
     private IEnumerator PickUpRandomVoice(string url) {
         Debug.Log(url);
-        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG);
+        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.OGGVORBIS);
         
         if (PlayerPrefs.GetInt("CanPlayMusic") == 0)
         {
@@ -213,7 +213,7 @@ public class VideoEngine : MonoBehaviour
     private IEnumerator PickUpRandomMusic(string url)
     {
         Debug.Log(url);
-        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG);
+        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.OGGVORBIS);
         AsyncOperation operation = request.SendWebRequest();
 
         if (PlayerPrefs.GetInt("CanPlayMusic") == 1)
