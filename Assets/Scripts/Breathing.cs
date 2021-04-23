3fb25cb2a8898287ae0bb76339a687c10d3c62c3
Ashot Danielyan <ashot111997@gmail.com>
2021-04-23 12:13:38 +0400

    added particles to video scene

--- Assets/Scripts/Breathing.cs
+++ Assets/Scripts/Breathing.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Breathing : MonoBehaviour
 {
     public ParticleSystem FrostEffect;
+    public ParticleSystem BreathIn;
+    public ParticleSystem BreathOut;
 
     public AudioSource BreathingSoundEffect;
 
     private void Start()
     {
-        if (PlayerPrefs.GetString("Breathing") == "on")
+        /*if (PlayerPrefs.GetString("Breathing") == "on")
         {
             BreathingSoundEffect.gameObject.SetActive(true);
             FrostEffect.gameObject.SetActive(true);
@@ -18,22 +20,41 @@ public class Breathing : MonoBehaviour
             BreathingSoundEffect.Pause();
             FrostEffect.Stop();
         }
-        else if ((PlayerPrefs.GetString("Breathing") == "off"))
+        else if (PlayerPrefs.GetString("Breathing") == "off")
         {
             BreathingSoundEffect.gameObject.SetActive(false);
             FrostEffect.gameObject.SetActive(false);
-        }
+        }*/
+
+        BreathingSoundEffect.gameObject.SetActive(true);
+        FrostEffect.Stop();
+        BreathIn.Stop();
+        BreathOut.Stop();
     }
 
     public IEnumerator StartBreathing()
     {
-        if (PlayerPrefs.GetString("Breathing") == "on")
+        /*if (PlayerPrefs.GetString("Breathing") == "on")
         {
             BreathingSoundEffect.Play();
             yield return new WaitForSeconds(4f);
             FrostEffect.Play();
             yield return new WaitForSeconds(5);
             StartCoroutine(StartBreathing());
+        }*/
+
+        BreathingSoundEffect.Play();
+        BreathingSoundEffect.volume = 0.05f;
+        if (PlayerPrefs.GetString("Breathing") == "vapor")
+        {
+
         }
+        else if (PlayerPrefs.GetString("Breathing") == "particle")
+        {
+
+        }
+
+        yield return new WaitForSeconds(9f);
+        StartCoroutine(StartBreathing());
     }
 }
